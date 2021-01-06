cfd6ea2fd233677e0b4cb5b04980188cab5e7665
Kevin Yamauchi <kevin.yamauchi@gmail.com>
2021-01-06 12:43:36 +0100

    demo notebook

--- setup.py
+++ setup.py
@@ -5,6 +5,6 @@ so this file is currently here to support "pip install -e ."
 from setuptools import setup
 
 setup(
-    use_scm_version={"write_to": "calciumcurator/_version.py"},
+    use_scm_version={"write_to": "morphologyanalysis/_version.py"},
     setup_requires=["setuptools_scm"],
 )
